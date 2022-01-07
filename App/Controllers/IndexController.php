24102e0327d87eac4a5df158d88a0ab605070b75
cangussu12 <leo.tywin@gmail.com>
2022-01-07 10:25:57 -0300

    FInalizando aula de Models e views

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -3,18 +3,33 @@
 namespace App\Controllers;
 
 use MF\Controller\Action;
+use MF\Model\Container;
+
+use App\Models\Produto;
+use App\Models\Info;
 
 class IndexController extends Action {
 
 	public function index() {
-		$this->view->dados = array('Sofá', 'Cadeira', 'Cama');
-		$this->render('index');
+
+		$produto = Container::getModel('Produto');
+
+		$produtos = $produto->getProdutos();
+
+		$this->view->dados = $produtos;
+		$this->render('index', 'layout1');
 	}
 
 	public function sobreNos() {
-		$this->view->dados = array('Notebook', 'Celular', 'PC');
-		$this->render('sobre_nos');
+
+		$info = Container::getModel('Info');
+		
+		$informacoes = $info->getInfo();
+
+		$this->view->dados = $informacoes;
+		$this->render('sobreNos', 'layout1');
 	}
+
 }
 
 
